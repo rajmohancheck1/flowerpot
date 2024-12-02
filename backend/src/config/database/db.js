e734989e13f74e561ba9c3c3804580b7f04be6a0
Rajmohan <rajmohanking20@gmail.com>
2024-12-02 10:25:32 +0530

    Final update

--- backend/src/config/database/db.js
+++ backend/src/config/database/db.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 mongoose.set("strictQuery", false);
 
 // console.log(process.env.COLLECTION_NAME);
-const DB = `mongodb+srv://${process.env.COLLECTION_NAME}:${process.env.COLLECTION_PASSWORD}@${process.env.COLLECTION_NAME}.cbqsaya.mongodb.net/?retryWrites=true&w=majority`;
+//const DB = `mongodb+srv://${process.env.COLLECTION_NAME}:${process.env.COLLECTION_PASSWORD}@${process.env.COLLECTION_NAME}.cbqsaya.mongodb.net/?retryWrites=true&w=majority`;
+
+const DB="mongodb://127.0.0.1:27017/flowerpot";
 
 mongoose.connect(DB, {
     useNewUrlParser: true,
